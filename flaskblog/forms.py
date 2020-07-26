d5ae513f068ff60d00eceef0ab40305cd0a79c5b
Siyu <sz2625@columbia.edu>
2020-07-26 14:27:19 -0400

    update

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -27,13 +27,13 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That email is taken. Please choose a different one.')
 
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
